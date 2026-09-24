2239221e8572228513eb95161ccc5a6bcdd20c93
jeeva26ai-beep <304818117+jeeva26ai-beep@users.noreply.github.com>
2026-09-24 10:39:29 +0530

    Update q6_is_nice_outside.py done

--- q6_is_nice_outside.py
+++ q6_is_nice_outside.py
@@ -1,12 +1,11 @@
 """ Question 6: Debug the Function """
 def fahrenheit_to_celsius(temp):
-    return (temp - 32) * (5 // 9)
+    return (temp - 32) * (5 / 9)
 
 def is_nice_outside(temperature, in_fahrenheit, is_raining):
     if in_fahrenheit:
         temperature = fahrenheit_to_celsius(temperature)
-    return (not is_raining) and ((temperature > 4) or (temperature < 35))
-
+    return (not is_raining) and (4 < temperature < 35)
 """ Test 6 """
 def test_is_nice_outside():
     print("Testing is_nice_outside...", end="")
