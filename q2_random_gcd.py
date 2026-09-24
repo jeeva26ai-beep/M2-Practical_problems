a32bbfb7a7b986f4f7b3e17ec3ce15508215ef80
jeeva26ai-beep <304818117+jeeva26ai-beep@users.noreply.github.com>
2026-09-24 10:25:25 +0530

    Update q2_random_gcd.py done

--- q2_random_gcd.py
+++ q2_random_gcd.py
@@ -1,11 +1,15 @@
+import math
+import random
 """ Question 2: random_gcd() """
 """
 Inputs: None
 Output: randomly generates two integers in range [1, 100] and returns gcd
 """
 def random_gcd():
-    return 
-
+    x = random.randint(1, 100)
+    y = random.randint(1, 100)
+    print(x, y)
+    return math.gcd(x, y)
 """ Test 2 """
 def test_random_gcd():
     print("Testing random_gcd...")
